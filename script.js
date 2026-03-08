635c77f2eb94e4a8d5a51216c875ebfc9deb3848
kobkiller26-design <kobkiller26@gmail.com>
2026-03-08 04:46:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 async function checkIMEI() {
 
 const imei = document.getElementById("imei").value
+const resultBox = document.getElementById("result")
 
 if(!imei){
 alert("Enter IMEI")
 return
 }
 
+resultBox.innerHTML = "Checking IMEI..."
+
+try{
+
 const response = await fetch("/api/check?imei=" + imei)
 const data = await response.json()
 
-document.getElementById("result").innerText = data.result
+resultBox.innerHTML = `
+<h3>IMEI Result</h3>
+<p>${data.result}</p>
+`
+
+}catch(error){
+
+resultBox.innerHTML = "Error checking IMEI"
+
+}
 
 }
