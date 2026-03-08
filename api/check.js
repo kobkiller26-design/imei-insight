832f586913743138c5a6403ff366a1c94a01bdfc
kobkiller26-design <kobkiller26@gmail.com>
2026-03-08 05:14:43 -0400

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,21 +1,44 @@
-export default async function handler(req, res) {
+import {verifyToken} from "../lib/auth.js"
+import {addHistory} from "../lib/db.js"
+import {limit} from "./ratelimit.js"
 
-const { imei } = req.query
+export default async function handler(req,res){
 
-const params = new URLSearchParams()
+const token=req.headers.authorization
 
-params.append("username","WilnoEsteril565")
-params.append("apiaccesskey","KKNGz-qcQhZ-VTkNe-u2DfL-jy020-vwdZQ")
+const user=verifyToken(token)
+
+if(!user){
+return res.status(401).json({error:"Login required"})
+}
+
+if(!limit(user.email)){
+return res.status(429).json({error:"Too many requests"})
+}
+
+const {imei}=req.query
+
+const params=new URLSearchParams()
+
+params.append("username","YOUR_DHRU_USERNAME")
+params.append("apiaccesskey","YOUR_DHRU_API_KEY")
 params.append("request","imei")
-params.append("imei", imei)
+params.append("imei",imei)
 
-const response = await fetch("https://dhru.checkimei.com/api/index.php",{
+const response=await fetch("https://dhru.checkimei.com/api/index.php",{
 method:"POST",
 body:params
 })
 
-const data = await response.text()
+const data=await response.text()
 
-res.status(200).json({result:data})
+addHistory({
+user:user.email,
+imei,
+result:data,
+date:new Date()
+})
+
+res.json({result:data})
 
 }
